dee3b84cf07138cced3c76fe928ce0ce9e239ca4
Ad <adhyan_srivastava95@yahoo.com>
2015-08-22 15:09:57 +0530

    Added comments

--- js/app.js
+++ js/app.js
@@ -1,29 +1,47 @@
-/* create the canvas element, grab the 2D context for that canvas
-* set the canvas elements height/width and add it to the DOM.
-*/
+/*Created By: Adhyan
+ *Date: 22-08-2015
+ *Aim: As part of Udacity's front-end nanodegree project.
+ *Place: Noida, India.
+ */
+
+/*app.js
+ *This file contains the functional class declaraion and 
+ *it's methods,which engine.js calls during each frame.
+	 
+ *This file contains globally available variables like
+ *flag, ctx, clockIsShowing etc, so that engine.js can 
+ *use it without any errors.
+ */
+
+/*create the canvas element, grab the 2D context for that canvas
+ *set the canvas elements height/width and add it to the DOM.
+ */
 var doc = document,
 	win = window,
-	canvas = doc.createElement('canvas'),
+	canvas = doc.createElement('canvas'), 	//This canvas element is for rendering game.
 	ctx = canvas.getContext('2d'),
 	numLevel = 1,
-	flag = 0,
-	enemyRandomPosition = [70, 140, 225],
-	c = doc.getElementById('timer'),
+	flag = 0, 								//This flag variable checks whether the player has been selected or not.
+	enemyRandomPosition = [70, 140, 225], 	//This list is for selecting random positons of enemy.
+	c = doc.getElementById('timer'), 		//This canvas element is for rendering clock.
 	cx = c.getContext('2d'),
-	clockIsShowing = 0;
+	clockIsShowing = 0; 					//This variable checks whether the clock is being shown or not.
 
 canvas.width = 505;
 canvas.height = 606;
 $(".canvas").append(canvas);
 
-/*The styling for the clock canvas*/
+//The styling for the clock canvas
 cx.strokeStyle = '#28d1fa';
 cx.lineWidth = 8;
 cx.lineCap = 'round';
 cx.shadowBlur = 10;
 cx.shadowColor = '#28d1fa';
 
-// Enemies our player must avoid
+/*
+ *@desc this functional class will hold the functions for enemy interactions.
+ *example include getRandomSpeed(), update(), render().		
+ */
 var Enemy = function(x, y) {
 	this.sprite = 'images/enemy-bug.png';
 	this.x = x;
@@ -31,109 +49,157 @@ var Enemy = function(x, y) {
 	this.speed = this.getRandomSpeed(100, 300);
 };
 
+/*
+ *@desc gets a random speed of the enemy.
+ *@params int min- the minimum speed of the enemy, int max- the maximum speed of the enemy.		
+ *@return int - the speed between min and max.		
+ */
 Enemy.prototype.getRandomSpeed = function(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/*
+ *@desc Update the enemy's position.
+ *@params float dt- a time delta between ticks			
+ */
 Enemy.prototype.update = function(dt) {
 	// You should multiply any movement by the dt parameter
 	// which will ensure the game runs at the same speed for
 	// all computers.
-	if (this.x > 505 ) {
+	if (this.x > 505) {
 		this.x = 0;
 	} else {
-		this.x = Math.floor(this.x + this.speed*dt);
+		this.x = Math.floor(this.x + this.speed * dt);
 	}
-}
+};
 
-// Draw the enemy on the screen, required method for game
+/*
+ *@desc Draw the enemy on the screen.		
+ */
 Enemy.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
+/*
+ *@desc this functional class will hold the functions for player interactions.
+ *example includes update(), render(), collision(), levelUp(), gameOver(), handleInput().		
+ */
 var Player = function(x, y, image) {
 	this.x = x;
 	this.y = y;
 	this.sprite = image;
 };
 
+/*
+ *@desc Draw the player on the screen.		
+ */
 Player.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
+/*
+ *@desc Update the player position.
+ *@params float dt- a time delta between ticks			
+ */
 Player.prototype.update = function(dt) {
 	if (this.direction == 'left' && this.x > 0) {
-		this.x -= dt*400;
+		this.x -= dt * 400;
 	} else if (this.direction == 'right' && this.x < 404) {
-		this.x += dt*400;
-	} else if (this.direction == 'down' && this.y < 75*4) {
-		this.y += dt*400;
+		this.x += dt * 400;
+	} else if (this.direction == 'down' && this.y < 75 * 4) {
+		this.y += dt * 400;
 	} else if (this.direction == 'up' && this.y > 0) {
-		this.y -= dt*400;
+		this.y -= dt * 400;
 	}
 	this.collision();
 	this.levelUp();
 	this.direction = '';
-}
+};
 
-Player.prototype.handleInput = function (key) {
+/*
+ *@desc sets which direction key was pressed.
+ *@params string key- direction name.			
+ */
+Player.prototype.handleInput = function(key) {
 	this.direction = key;
-}
+};
 
+/*
+ *@desc checks whether the player is collided with the enemy or not.
+ */
 Player.prototype.collision = function() {
-	var pX , pY, bX, bY;
-	pX = player.x;
-	pY = player.y; 
+	var pX, pY, bX, bY;
+	//player's current x and y coordinates.
+	pX = this.x;
+	pY = this.y;
 	for (var i = 0; i < allEnemies.length; i++) {
+		//enemy current x and y coordinates.
 		bX = allEnemies[i].x;
 		bY = allEnemies[i].y;
-		if (pX < (bX + 101) && bX < (pX + 101) && pY < (bY + 70) && bY < (pY + 70)){
-			player.x = 101*2;
-			player.y = 75*4;
+		if (pX < (bX + 101) && bX < (pX + 101) && pY < (bY + 70) && bY < (pY + 70)) {
+			this.x = 101 * 2;
+			this.y = 75 * 4;
 		}
 	}
-}
+};
 
+/*
+ *@desc increases the level if player reaches the water.
+ */
 Player.prototype.levelUp = function() {
 	if (player.y <= 0) {
 		Materialize.toast("Level Completed!", 1000, 'rounded');
-		player.x = 101*2;
-		player.y = 75*4;
+		this.x = 101 * 2;
+		this.y = 75 * 4;
 		numLevel += 1;
-		var enemy = new Enemy(0, enemyRandomPosition[Math.floor(Math.random()*3)]);
+		var enemy = new Enemy(0, enemyRandomPosition[Math.floor(Math.random() * 3)]);
 		allEnemies.push(enemy);
 		$('.level').text(numLevel);
 		clock.reset();
 	}
-}
+};
 
+/*
+ *@desc restarts the game from 1st level if 60 seconds have been passed.
+ */
 Player.prototype.gameOver = function() {
-	var enemy = new Enemy(0, enemyRandomPosition[Math.floor(Math.random()*3)]);
+	var enemy = new Enemy(0, enemyRandomPosition[Math.floor(Math.random() * 3)]);
 	allEnemies = [enemy];
 	numLevel = 1;
 	$('.level').text(numLevel);
-	player.x = 101*2;
-	player.y = 75*4;
+	this.x = 101 * 2;
+	this.y = 75 * 4;
 	clock.reset();
-}
+};
 
+/*
+ *@desc this functional class will hold the functions for clock interactions.
+ *example includes init(), reset(), tick(), show(), degToRad(), renderTime().		
+ */
 var Clock = function() {
 	this.start_time = 59;
 	this.name = "clock";
-}
+};
 
+/*
+ *@desc initialize the clock.	
+ */
 Clock.prototype.init = function() {
 	this.reset();
 	setInterval(this.name + '.tick()', 1000);
-}
+};
 
+/*
+ *@desc reset the clock.	
+ */
 Clock.prototype.reset = function() {
 	this.seconds = this.start_time;
 	this.renderTime();
-}
+};
 
+/*
+ *@desc decreases the clock time by 1 second.	
+ */
 Clock.prototype.tick = function() {
 	if (this.seconds >= 0) {
 		this.seconds--;
@@ -145,61 +211,71 @@ Clock.prototype.tick = function() {
 		}
 	}
 	this.renderTime();
-}
+};
 
+/*
+ *@desc initial display of the clock when the player has not been selected.	
+ */
 Clock.prototype.show = function() {
-	this.seconds = 00;
+	this.seconds = 0;
 	this.renderTime();
-}
+};
 
+/*
+ *@desc convers degree to radians.	
+ */
 Clock.prototype.degToRad = function(degree) {
-	var factor = (Math.PI)/180;
-	return degree*factor;
-}
+	var factor = (Math.PI) / 180;
+	return degree * factor;
+};
 
+/*
+ *@desc shows the clock on the canvas.	
+ */
 Clock.prototype.renderTime = function() {
 	cx.fillStyle = '#566E7A';
 	cx.fillRect(0, 0, 370, 170);
 	cx.beginPath();
-	cx.arc(170, 85, 70, this.degToRad(270), this.degToRad((this.seconds*6)-90));
+	cx.arc(170, 85, 70, this.degToRad(270), this.degToRad((this.seconds * 6) - 90));
 	cx.stroke();
 	cx.font = "28px Arial";
 	cx.fillStyle = '#28d1fa';
 	if (this.seconds < 10) {
-		cx.fillText('0'+this.seconds, 156, 95);
+		cx.fillText('0' + this.seconds, 156, 95);
 	} else {
 		cx.fillText(this.seconds, 156, 95);
 	}
-}
+};
 
 //Initializing the player, enemy and clock objects.
-
-var enemy = new Enemy(0, enemyRandomPosition[Math.floor(Math.random()*3)]);
+var enemy = new Enemy(0, enemyRandomPosition[Math.floor(Math.random() * 3)]);
 var player;
 var allEnemies = [enemy];
 var clock = new Clock();
 
-//This listens for click on the player.
-
+/*
+ *@desc listens for click on the player.
+ */
 canvas.addEventListener('click', function(e) {
 	var x = e.x - canvas.offsetLeft;
 	var y = e.y - canvas.offsetTop - 54;
-	if (y > 83*5 && y <= 83*6) {
-		if (x > 101 && x <= 202 ) {
+	if (y > 83 * 5 && y <= 83 * 6) {
+		if (x > 101 && x <= 202) {
 			flag = 1;
-			player = new Player(101*2, 75*4, 'images/char-boy.png');
-		} else if(x > 202 && x <= 303)  {
+			player = new Player(101 * 2, 75 * 4, 'images/char-boy.png');
+		} else if (x > 202 && x <= 303) {
 			flag = 1;
-			player = new Player(101*2, 75*4, 'images/char-cat-girl.png');			
-		} else if(x > 303 && x <= 404)  {
+			player = new Player(101 * 2, 75 * 4, 'images/char-cat-girl.png');
+		} else if (x > 303 && x <= 404) {
 			flag = 1;
-			player = new Player(101*2, 75*4, 'images/char-horn-girl.png');			
+			player = new Player(101 * 2, 75 * 4, 'images/char-horn-girl.png');
 		}
 	}
 });
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method.
+/*
+ *@desc This listens for key presses and sends the keys to your Player.handleInput() method.
+ */
 document.addEventListener('keydown', function(e) {
 	var allowedKeys = {
 		37: 'left',
--- js/engine.js
+++ js/engine.js
@@ -1,18 +1,16 @@
-/* Engine.js
- * This file provides the game loop functionality (update entities and render),
- * draws the initial game board on the screen, and then calls the update and
- * render methods on your player and enemy objects (defined in your app.js).
- *
- * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
- * the screen, it may look like just that image/character is moving or being
- * drawn but that is not the case. What's really happening is the entire "scene"
- * is being drawn over and over, presenting the illusion of animation.
- *
- * This engine is available globally via the Engine variable and it also makes
- * the canvas' context (ctx) object globally available to make writing app.js
- * a little simpler to work with.
- */
+/*	Engine.js
+	* This file provides the game loop functionality (update entities and render),
+	* draws the initial game board on the screen, and then calls the update and
+	* render methods on your player and enemy objects (defined in your app.js).
+	*
+	* A game engine works by drawing the entire game screen over and over, kind of
+	* like a flipbook you may have created as a kid. When your player moves across
+	* the screen, it may look like just that image/character is moving or being
+	* drawn but that is not the case. What's really happening is the entire "scene"
+	* is being drawn over and over, presenting the illusion of animation.
+	*
+	* This engine is available globally via the Engine variable.
+*/
 
 var Engine = (function() {
 	/* Predefine the variables we'll be using within this scope*/
@@ -45,7 +43,7 @@ var Engine = (function() {
 		 * function again as soon as the browser is able to draw another frame.
 		 */
 		win.requestAnimationFrame(main);
-	};
+	}
 
 	/* This function does some initial setup that should only occur once,
 	 * particularly setting the lastTime variable that is required for the
